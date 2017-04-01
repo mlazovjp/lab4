32c9cb4742aae2d8d0b2f30a99c113e388062efe
mlazovjp <noone@null.com>
2017-03-31 20:31:08 -0400

    trying to fix comparison of export to function name to find

--- script03-detectExportsCallingSpecificFunctions.py
+++ script03-detectExportsCallingSpecificFunctions.py
@@ -42,61 +42,63 @@ def getListOfAllFunctionsEAs(listOfAllFunctionsEAs):
 # recursively traverse calling functions and hope the trail ends with an export function
 def processFunction(targetedFuncEA, currentFuncEA, listOfAllExportsEAs):
 	listOfFunctionsEAsCallingThisFunction.append( CodeRefsTo(currentFuncEA, 0) )
+	print("targetedFuncEA[%x], currentFuncEA[%x]:") % (targetedFuncEA, currentFuncEA)
+	print[hex(ea) for ea in listOfFunctionsEAsCallingThisFunction]
 	
 	# if no functions call this function ....
-	if not listOfFunctionsEAsCallingThisFunction:
-		return
+	#if not listOfFunctionsEAsCallingThisFunction:
+	#	return
 	
 	# continue processing
-	for fctfEA in listOfFunctionsEAsCallingThisFunction:
+	#for fctfEA in listOfFunctionsEAsCallingThisFunction:
 	
 		# reset boolean ... no export found in this pass
-		exportFound = False
+	#	exportFound = False
 		
 		# cycle through list of all exports
-		for anExport in listOfAllExportsEAs:
+	#	for anExport in listOfAllExportsEAs:
 		
 			# get the name of the export
-			exportName = (GetFunctionName(anExport))
+	#		exportName = (GetFunctionName(anExport))
 			
 			# get name of the function
-			fctfName = GetFunctionName(fctfEA)
+	#		fctfName = GetFunctionName(fctfEA)
 			
 			# compare name of the current "function calling this function" to the list of names of exports
 			# we haev to do some trickery to parse the "clean" version of the function
-			if exportName.find("_strcpy") > -1:
-				if len(exportName) == (exportName.find("_strcpy") + len("_strcpy")):		# nothing after "_strcpy"
-					exportFound = True
-					print("%s:strcpy") % exportName
-					continue
+	#		if exportName.find("_strcpy") > -1:
+	#			if len(exportName) == (exportName.find("_strcpy") + len("_strcpy")):		# nothing after "_strcpy"
+	#				exportFound = True
+	#				print("%s:strcpy") % exportName
+	#				continue
 					
-			elif exportName.find("_sprintf") > -1:
-				if len(exportName) == (exportName.find("_sprintf") + len("_sprintf")):		# nothing after "_sprintf"
-					exportFound = True
-					print("%s:sprintf") % exportName
-					continue
+	#		elif exportName.find("_sprintf") > -1:
+	#			if len(exportName) == (exportName.find("_sprintf") + len("_sprintf")):		# nothing after "_sprintf"
+	#				exportFound = True
+	#				print("%s:sprintf") % exportName
+	#				continue
 					
-			elif exportName.find("_strncpy") > -1:
-				if len(exportName) == (exportName.find("_strncpy") + len("_strncpy")):		# nothing after "_strncpy"
-					exportFound = True
-					print("%s:strncpy") % exportName
-					continue
+	#		elif exportName.find("_strncpy") > -1:
+	#			if len(exportName) == (exportName.find("_strncpy") + len("_strncpy")):		# nothing after "_strncpy"
+	#				exportFound = True
+	#				print("%s:strncpy") % exportName
+	#				continue
 					
-			elif exportName.find("_wcsncpy") > -1:
-				if len(exportName) == (exportName.find("_wcsncpy") + len("_wcsncpy")):		# nothing after "_wcsncpy"
-					exportFound = True
-					print("%s:wcsncpy") % exportName
-					continue
+	#		elif exportName.find("_wcsncpy") > -1:
+	#			if len(exportName) == (exportName.find("_wcsncpy") + len("_wcsncpy")):		# nothing after "_wcsncpy"
+	#				exportFound = True
+	#				print("%s:wcsncpy") % exportName
+	#				continue
 					
-			elif exportName.find("_swprintf") > -1:
-				if len(exportName) == (exportName.find("_swprintf") + len("_swprintf")):	# nothing after "_swprintf"
-					exportFound = True
-					print("%s:swprintf") % exportName
-					continue
+	#		elif exportName.find("_swprintf") > -1:
+	#			if len(exportName) == (exportName.find("_swprintf") + len("_swprintf")):	# nothing after "_swprintf"
+	#				exportFound = True
+	#				print("%s:swprintf") % exportName
+	#				continue
 					
 					
-			if exportFound == False:
-				processFunction(targetedFuncEA, fctfEA, listOfAllExportsEAs)
+	#		if exportFound == False:
+	#			processFunction(targetedFuncEA, fctfEA, listOfAllExportsEAs)
 
 
 		
